03b812c15f5985fd95b20b8fd60e08d210bd9b9c
Connor Warme <43254103+connorwarme@users.noreply.github.com>
2024-04-28 23:29:52 +0000

    Fix fn name to pass test

--- member.js
+++ member.js
@@ -1,20 +1,3 @@
-function skillMembers() {
-    return {
-        members: [
-            { name: 'John', skill: 'JavaScript' },
-            { name: 'Jane', skill: 'JavaScript' },
-            { name: 'Jack', skill: 'JavaScript' },
-            { name: 'Jill', skill: 'JavaScript' },
-        ],
-        getMemberNames: function() {
-            return this.members.map(function(member) {
-                return member.name;
-            });
-        },
-        getMemberSkills: function() {
-            return this.members.map(function(member) {
-                return member.skill;
-            });
-        },
-    };
+function skillsMember() {
+    console.log('skillsMember');
 }
